104431df5e4ef7b481b22e7268a9f3076c57baa3
Space555 <5arhipoff5@mail.ru>
2023-05-26 18:20:34 +0600

    Commit

---
+++ mysite/index/static/index/js/index.js
@@ -0,0 +1,56 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+        delay: 3000,
+    },
+    speed: 1000,
+    disableOnInteraction: false,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+
+  $(".django__acc").accordion({
+    heightStyle: "content",
+    active: 0,
+    collapsible: true,
+  });
+
+    window.addEventListener('DOMContentLoaded', function() {
+        document.querySelector('.fulls').addEventListener('click', function() {
+            document.querySelector('.full').classList.toggle('is-open')
+        })
+
+        document.querySelector('.reversed').addEventListener('click', function() {
+            document.querySelector('.reverse').classList.toggle('is-open')
+        })
+
+        document.querySelector('.securited').addEventListener('click', function() {
+            document.querySelector('.security').classList.toggle('is-open')
+        })
+
+        document.querySelector('.scaled').addEventListener('click', function() {
+            document.querySelector('.scale').classList.toggle('is-open')
+        })
+
+        document.querySelector('.comforted').addEventListener('click', function() {
+            document.querySelector('.comfort').classList.toggle('is-open')
+        })
+
+        document.querySelector('.portabled').addEventListener('click', function() {
+            document.querySelector('.portable').classList.toggle('is-open')
+        })
+    })
+
+
